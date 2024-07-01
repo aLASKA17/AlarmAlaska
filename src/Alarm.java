2943332641f5e67804fdbaec8c7e08d6c90e67c8
a1aska17 <92105945+aLASKA17@users.noreply.github.com>
2024-07-01 16:18:12 +0600

    Create Project

---
+++ src/Alarm.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Alarm {
+    public static  void alarmWindow(){
+        JFrame mainWindow = new JFrame("AlarmAlaska");
+        mainWindow.setSize(500,1000);
+
+
+        mainWindow.setVisible(true);
+    }
+}
